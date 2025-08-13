f878d19b8d039b533e0d578dabf2638d7467a7b2
Newfie <157941859+Newfies@users.noreply.github.com>
2025-08-12 23:40:12 -0500

    x

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ document.addEventListener("DOMContentLoaded", function(){
         event.preventDefault();
     })
 
+    const YouTube = document.getElementById('youtube');
+    const Twitch = document.getElementById('twitch');
+    const Patreon = document.getElementById('patreon');
+    const Discord = document.getElementById('discord');
+
+    YouTube.addEventListener('click', function(){
+        window.open("https://youtube.com/@GvNx");
+    })
+
+    Twitch.addEventListener('click', function(){
+        window.open("https://twitch.tv/SimplyGav");
+    })
+
+    Patreon.addEventListener('click', function(){
+        window.open("https://patreon.com/GvNx");
+    })
+
+    Discord.addEventListener('click', function(){
+        window.open("https://dsc.gg/simplygav");
+    })
+
 })
